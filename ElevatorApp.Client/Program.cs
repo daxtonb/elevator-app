098487df859b1c89710b924d4f38da3207ac3456
Daxton Burgon <daxtonburgon@gmail.com>
2021-10-16 16:13:40 -0600

    Init

---
+++ ElevatorApp.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElevatorApp.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ElevatorApp.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ElevatorApp.Core
+{
+    public class Class1
+    {
+    }
+}
