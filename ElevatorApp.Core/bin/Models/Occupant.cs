0aa53d43121e3ebe031467c9f3eb163b7de99481
Daxton Burgon <daxtonburgon@gmail.com>
2021-10-16 16:14:47 -0600

    Added models

---
+++ ElevatorApp.Core/bin/Models/Occupant.cs
@@ -0,0 +1,27 @@
+namespace Elevator.Core
+{
+    /// <summary>
+    /// Represents a building occupant that can ride in elevator
+    /// </summary>
+    public class Occupant
+    {
+        /// <summary>
+        /// Weight in lbs of occupant
+        /// </summary>
+        public double Weight { get; }
+        /// <summary>
+        /// Floor that occupant is currently on. Default value is 1 (ground floor)
+        /// </summary>
+        public int CurrentFloor { get; set; } = 1;
+        /// <summary>
+        /// Floor that occupant wishes to ride elevator to
+        /// </summary>
+        public int RequestedFloor { get; set; }
+
+        /// <param name="weight">Weight of occupant</param>
+        public Occupant(double weight)
+        {
+            Weight = weight;
+        }
+    }
+}
