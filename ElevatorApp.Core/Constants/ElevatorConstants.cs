205d6d5bce4782a87513573ebd8aa8abf536326a
Daxton Burgon <daxtonburgon@gmail.com>
2021-10-25 02:36:28 -0600

    Changed elapse time to 500

--- ElevatorApp.Core/Constants/ElevatorConstants.cs
+++ ElevatorApp.Core/Constants/ElevatorConstants.cs
@@ -3,7 +3,7 @@ namespace ElevatorApp.Core
     public static class ElevatorConstants
     {
         public static readonly int TIME_TO_CLOSE_DOORS = 1; // 1 s
-        public static readonly int ELAPSE_TIME = 1000;  // 1000 ms (1 s)
+        public static readonly int ELAPSE_TIME = 500;  // 500 ms (0.5 s)
         public static readonly int MAX_SPEED = 3;   // 3 ft/s
         public static readonly double FLOOR_HEIGHT = 9; // 9 ft
 
