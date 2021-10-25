797469fe7d5398915134e26da3a91fe895c6f07f
Daxton Burgon <daxtonburgon@gmail.com>
2021-10-24 20:22:36 -0600

    Added tests

--- ElevatorApp.Test/ElevatorTests.cs
+++ ElevatorApp.Test/ElevatorTests.cs
@@ -47,21 +47,107 @@ namespace ElevatorApp.Test
         [Fact]
         public async void Occupant_Can_Enter_Stopped_Elevator_On_Same_Floor()
         {
-            // Given
             var occupant = factory.CreateOccupant();
+            await TestIsInElevatorAsync(occupant);
+        }
+
+        [Fact]
+        public async void Occupant_Can_Request_Floor_Inside_Elevator()
+        {
+            var occupant = factory.CreateOccupant();
+            int floorNumber = 2;
+
+            await TestElevatorIsMovingAsync(occupant, floorNumber);
+        }
+
+        [Fact]
+        public async void Occupant_Exits_Elevator_On_Requested_Floor()
+        {
+            var occupant = factory.CreateOccupant();
+            int floorNumber = 2;
+            
+            await TestOccupantExitsOnRequestedFloor(occupant, floorNumber);
+        }
+
+        private async Task TestIsInElevatorAsync(Occupant occupant)
+        {
+            // Given
             bool isInElevator = false;
+            Occupant.StateChangedHandler onStateChanged = (Occupant occupant, Occupant.StateChangedEventArgs eventArgs) => 
+            {
+                isInElevator = eventArgs.State == Occupant.State.riding && occupant.Elevator != null;
+            };
+            occupant.StateChanged += onStateChanged;
 
             // When
             await occupant.RequestElevatorAsync(Elevator.Direction.Up);
-            occupant.StateChanged += (Occupant occupant, Occupant.StateChangedEventArgs eventArgs) => 
-            {
-                isInElevator = eventArgs.State == Occupant.State.riding;
-            };
-            
-            Thread.Sleep(30000);
+
+            Thread.Sleep(ElevatorConstants.ELAPSE_TIME * 10 + 1000);
 
             // Then
-            Assert.True(isInElevator, "Occupant did not board the elevator");
+            Assert.True(isInElevator, $"Occupant did not board the elevator - State: {occupant.CurrentState}, In elevator: {occupant.Elevator != null}");
+
+            occupant.StateChanged -= onStateChanged;
+        }
+
+        private async Task TestElevatorIsMovingAsync(Occupant occupant, int floorNumber)
+        {
+            // Given
+            bool isElevatorMoving = false;
+            Elevator.StateChangedHandler onElevatorStateChanged = (Elevator elevator, Elevator.StateChangedEventArgs eventArgs) => 
+            {
+                isElevatorMoving = elevator.CurrentState == Elevator.State.Moving;
+            };
+
+            // When
+            await TestIsInElevatorAsync(occupant);
+            occupant.Elevator.StateChanged += onElevatorStateChanged;
+            await occupant.RequestFloorAsync(floorNumber);
+            Thread.Sleep(ElevatorConstants.TIME_TO_CLOSE_DOORS * 1000 + ElevatorConstants.ELAPSE_TIME);
+
+            // Then
+            Assert.True(isElevatorMoving, "The occupant's request was not acknowledged.");
+
+            occupant.Elevator.StateChanged -= onElevatorStateChanged;
+        }
+
+        private async Task TestOccupantExitsOnRequestedFloor(Occupant occupant, int floorNumber)
+        {
+            // Given
+            bool isElevatorStopped = false;
+            bool isElevatorEmpty = false;
+            bool isOccupantOutsideElevator = false;
+
+            Elevator.StateChangedHandler onElevatorStateChanged1 = (Elevator elevator, Elevator.StateChangedEventArgs eventArgs) => 
+            {
+                isElevatorStopped = elevator.CurrentState != Elevator.State.Moving;
+            };
+            Elevator.StateChangedHandler onElevatorStateChanged2 = (Elevator elevator, Elevator.StateChangedEventArgs eventArgs) => 
+            {
+                isElevatorEmpty = elevator.OccupantsCount == 0;
+            };
+            Occupant.StateChangedHandler onOccupantStateChanged = (Occupant occupant, Occupant.StateChangedEventArgs eventArgs) => 
+            {
+                isOccupantOutsideElevator = occupant.CurrentFloor == floorNumber && occupant.CurrentState == Occupant.State.none && occupant.Elevator == null;
+            };
+
+            // When
+            await TestIsInElevatorAsync(occupant);
+            await TestElevatorIsMovingAsync(occupant,floorNumber);
+            occupant.StateChanged += onOccupantStateChanged;
+            occupant.Elevator.StateChanged += onElevatorStateChanged1;
+            occupant.Elevator.StateChanged += onElevatorStateChanged2;
+
+            Thread.Sleep((floorNumber - 1) * Convert.ToInt32(ElevatorConstants.FLOOR_HEIGHT) / ElevatorConstants.MAX_SPEED + ElevatorConstants.ELAPSE_TIME);
+
+            // Then
+            Assert.True(isElevatorStopped, "Elevator is not stopped");
+            Assert.True(isElevatorEmpty, "Elevator is not empty");
+            Assert.True(isOccupantOutsideElevator, "Occupant is still inside of elevator.");
+
+            occupant.StateChanged -= onOccupantStateChanged;
+            occupant.Elevator.StateChanged -= onElevatorStateChanged1;
+            occupant.Elevator.StateChanged -= onElevatorStateChanged2;
         }
     }
 }
